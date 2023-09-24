db1c772fd4a3a8ea183c3b31f535f51905e23d2b
fabi-cm <dahofcmmejia@gmail.com>
2023-09-24 19:15:11 -0400

    45. en la clase contacto se agrego metodo HTTP seguros

--- store/views/contacto.py
+++ store/views/contacto.py
@@ -5,7 +5,9 @@ from store.forms.perfilForms import ContactoForms, ContactoMensajeForms
 from django.core.mail import EmailMessage
 from django.contrib import messages
 from twilio.rest import Client
+from django.views.decorators.http import require_http_methods
 
+@require_http_methods(["POST"])
 def contacto(request):
     # print('Tipo de petición: {}'.format(request.method))
     contact_form = ContactoForms()
